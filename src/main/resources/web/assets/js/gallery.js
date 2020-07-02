9cd0916743aaeb1ab0202f60a9705b22a5a2b7e4
Cristian <cristian.giannetti@mail.polimi.it>
2020-07-03 01:33:14 +0200

    Add Show and Delete buttons on gallery grid

--- src/main/resources/web/assets/js/gallery.js
+++ src/main/resources/web/assets/js/gallery.js
@@ -11,20 +11,47 @@ document.addEventListener('DOMContentLoaded', async function() {
 function addImage(item){
     const previewUrl = item.previewUrl;
     let div = document.createElement('div');
-    div.id = 'container';
+    div.id = 'container' + item.id;
     div.className = "col-lg-3 col-md-4 col-6";
-    div.innerHTML = "<a href=\"#\" class=\"d-block mb-4 h-100\">\n" +
-        "<img class=\"img-fluid img-thumbnail\" src=\"" + previewUrl + "\" alt=\"\">\n" +
-        "</a>";
+    div.innerHTML = "<a href='#' class='d-block mb-4 h-100\'>\n" +
+        "<img class='img-fluid img-thumbnail preview-image' src='" + previewUrl + "' alt=''>\n" +
+        "</a>\n";
+
+    document.getElementById("imageContainer").appendChild(div);
+    addShowButton(item);
+    addDeleteButton(item);
+}
+
+function addShowButton(item){
     const url = item.url;
     const imageName = item.name;
 
-    let newModal = function() {
-        openModal(url, imageName)
-    };
+    let showButton = document.createElement('button');
+    showButton.id = 'showButton';
+    showButton.className = 'btn btn-primary btn-sm';
+    showButton.innerHTML = '<img src="../assets/img/icon/show.svg"> Show';
 
-    div.onclick = newModal;
-    document.getElementById("imageContainer").appendChild(div);
+    let showModal = function() {
+        openModal(url, imageName);
+    };
+    showButton.onclick = showModal;
+    document.getElementById("container" + item.id).appendChild(showButton);
+}
+
+function addDeleteButton(item){
+    const url = item.url;
+
+    let deleteButton = document.createElement('button');
+    deleteButton.id = 'deleteButton';
+    deleteButton.className = 'btn btn-danger btn-sm';
+    deleteButton.innerHTML = '<img src="../assets/img/icon/bin.svg"> Delete';
+
+    let deleteImageOnClick = function() {
+        deleteImage(item);
+    };
+    deleteButton.onclick = deleteImageOnClick;
+
+    document.getElementById("container" + item.id).appendChild(deleteButton);
 }
 
 function openModal(url, name){
@@ -44,5 +71,21 @@ function openModal(url, name){
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
+    }
+}
+
+async function deleteImage(item) {
+    const options = {
+        method: 'DELETE',
+    };
+    try {
+        let response = await fetch("/api/images/" + item.id, options);
+        console.log(response.status);
+        let status = response.status;
+        if (status === 200) {
+            location.reload();
+        }
+    } catch (e) {
+        console.log("An error occured!");
     }
 }
